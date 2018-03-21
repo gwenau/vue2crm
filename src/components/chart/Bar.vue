8db20df936f80d71ca09596e0d66d79992c043f4
Gwen Au <quyen_au@hotmail.com>
2018-03-21 19:49:48 +1100

    Bug fixed. Had to put the times object in the correct area to collect the right values

--- src/components/chart/Bar.vue
+++ src/components/chart/Bar.vue
@@ -46,7 +46,6 @@
         startTime: moment(),
         endTime: moment(),
         times: [],
-        times_object: [],
         colours: ['#e6194b', '#3cb44b', '#ffe119', '#0082c8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#d2f53c', '#fabebe', '#008080', '#e6beff', '#aa6e28', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000080', '#808080'],
         items: []
       }
@@ -66,20 +65,22 @@
           timeStops.push(moment(this.startTime).format('HH:mm'))
           this.startTime.add(interval, 'minutes')
         }
-        this.times = timeStops
+        return timeStops
       },
       getPageViews(startDate = '2017-06-01T00:00:00.000Z', endDate = '2017-06-02T00:00:00.000Z', interval = 30) {
-        this.getTimeStops('00:00', '23:59', interval)
         this.api.getData(`page_view.json?start_date=${startDate}&end_date=${endDate}&interval=${interval}`).then((response) => {
+          let timesObject = []
           this.items = response.data
           this.datacollection = Object.assign({})
           this.$set(this.datacollection, 'labels', [])
           this.$set(this.datacollection, 'datasets', [])
+          this.times = Object.assign([])
+          this.times = this.getTimeStops('00:00', '23:59', interval)
           this.items.forEach((item, i) => {
             let pageUrlAndViews = {type: 'bar', label: item.key, backgroundColor: this.colours[i], data: []}
-            let buckets = item.group_by_half_hour.buckets
+            let buckets = item.group_by_time_interval.buckets
             buckets.forEach((bucket) => {
-              let hash = this.times_object
+              let hash = timesObject
               let time = moment(bucket.key).format('HH:mm')
               hash[time] = bucket.doc_count
               let newArrayDataOfOjbect = Object.values(hash)
